8b296bd4eeff76c4e12e6326b07f3c70c9fc703a
hirusha-kiranidu <hirusha.20233001@iit.ac.lk>
2026-01-04 22:14:57 +0530

    all suitable css fils added. define the browserRoutes in

--- estate-agent-app/src/App.jsx
+++ estate-agent-app/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
+import PropertyPage from "./pages/PropertyPage";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-      <h1>Estate Agent Application</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/property/:id" element={<PropertyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
